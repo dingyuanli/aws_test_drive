8423fd4fbe6309fdedb09e118fef9c39e0af6172
Li <dingyuan@e0b55fe8e1f2.ant.amazon.com>
2020-10-15 13:39:36 +0800

    made some modification

--- awscli/hello_world/app.py
+++ awscli/hello_world/app.py
@@ -1,42 +1,21 @@
-import json
+import subprocess
+import logging
 
-# import requests
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
+def run_command(command):
+    command_list = command.split(' ')
+
+    try:
+        logger.info("Running shell command: \"{}\"".format(command))
+        result = subprocess.run(command_list, stdout=subprocess.PIPE);
+        logger.info("Command output:\n---\n{}\n---".format(result.stdout.decode('UTF-8')))
+    except Exception as e:
+        logger.error("Exception: {}".format(e))
+        return False
+
+    return True
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
-
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
-
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello world",
-            # "location": ip.text.replace("\n", "")
-        }),
-    }
+    run_command('/opt/aws --version')
